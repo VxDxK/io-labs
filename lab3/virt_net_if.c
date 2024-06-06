583114e3ed35d0c6a63670110ad7ec291a8e4893
vadim <vadim.v.ponomarev@gmail.com>
2024-06-06 23:39:16 +0300

    update packet handler

--- lab3/virt_net_if.c
+++ lab3/virt_net_if.c
@@ -8,10 +8,10 @@
 #include <linux/ip.h>
 #include <linux/udp.h>
 
-static char* link = "lo";
+static char *link = "lo";
 module_param(link, charp, 0);
 
-static char* ifname = "vni%d";
+static char *ifname = "vni%d";
 static unsigned char data[1500];
 
 static struct net_device_stats stats;
@@ -21,26 +21,49 @@ struct priv {
     struct net_device *parent;
 };
 
-static char check_frame(struct sk_buff *skb, unsigned char data_shift) {
+static char check_frame(struct sk_buff *skb, unsigned char data_shift)
+{
     unsigned char *user_data_ptr = NULL;
     struct iphdr *ip = (struct iphdr *)skb_network_header(skb);
 
-    if (ip && ip->version == 4 && ip->protocol == 17) {
+    if (ip && ip->version == 4 && ip->protocol == IPPROTO_UDP) {
         unsigned char end_user = 255 & (ntohl(ip->daddr));
         if (end_user != 0 && end_user != 255) {
-            printk("Captured IPv4 packet, source: %d.%d.%d.%d, destination: %d.%d.%d.%d",
-                   ntohl(ip->saddr) >> 24, (ntohl(ip->saddr) >> 16) & 0x00FF,
-                   (ntohl(ip->saddr) >> 8) & 0x0000FF, (ntohl(ip->saddr)) & 0x000000FF,
-                   ntohl(ip->daddr) >> 24, (ntohl(ip->daddr) >> 16) & 0x00FF,
-                   (ntohl(ip->daddr) >> 8) & 0x0000FF, (ntohl(ip->daddr)) & 0x000000FF);
+            struct udphdr *udp = udp_hdr(skb);
+            u16 payload_length = ntohs(udp->len) - sizeof(struct udphdr);
+            user_data_ptr = (unsigned char *)(skb->data + sizeof(struct iphdr) +
+                                              sizeof(struct udphdr)) +
+                            data_shift;
+            if(payload_length > 1500) {
+                return 1;
+                pr_warn("udp length > 1500");
+            }
+            memcpy(data, user_data_ptr, payload_length);
+            if(strstr(user_data_ptr, "panic me") != NULL) {
+                pr_info("Panicing");
+                panic("Lol, gl with kernel recovering");
+            }
+            if (strstr(user_data_ptr, "this packet") == NULL) {
+                return 1;
+            }
+            pr_info(
+                "Captured IPv4 with UDP packet, source: %d.%d.%d.%d:%hu, destination: %d.%d.%d.%d:%hu\n  data length: %hu, data: %s",
+                ntohl(ip->saddr) >> 24, (ntohl(ip->saddr) >> 16) & 0x00FF,
+                (ntohl(ip->saddr) >> 8) & 0x0000FF,
+                (ntohl(ip->saddr)) & 0x000000FF, ntohs(udp->source),
+                ntohl(ip->daddr) >> 24, (ntohl(ip->daddr) >> 16) & 0x00FF,
+                (ntohl(ip->daddr) >> 8) & 0x0000FF,
+                (ntohl(ip->daddr)) & 0x000000FF, ntohs(udp->dest),
+                payload_length, user_data_ptr);
+
             return 1;
         }
     }
     return 0;
 }
 
-static rx_handler_result_t handle_frame(struct sk_buff **pskb) {
-
+static rx_handler_result_t handle_frame(struct sk_buff **pskb)
+{
     if (check_frame(*pskb, 0)) {
         stats.rx_packets++;
         stats.rx_bytes += (*pskb)->len;
@@ -49,19 +72,22 @@ static rx_handler_result_t handle_frame(struct sk_buff **pskb) {
     return RX_HANDLER_ANOTHER;
 }
 
-static int open(struct net_device *dev) {
+static int open(struct net_device *dev)
+{
     netif_start_queue(dev);
     printk(KERN_INFO "%s: device opened", dev->name);
     return 0;
 }
 
-static int stop(struct net_device *dev) {
+static int stop(struct net_device *dev)
+{
     netif_stop_queue(dev);
     printk(KERN_INFO "%s: device closed", dev->name);
     return 0;
 }
 
-static netdev_tx_t start_xmit(struct sk_buff *skb, struct net_device *dev) {
+static netdev_tx_t start_xmit(struct sk_buff *skb, struct net_device *dev)
+{
     struct priv *priv = netdev_priv(dev);
 
     if (check_frame(skb, 14)) {
@@ -78,29 +104,31 @@ static netdev_tx_t start_xmit(struct sk_buff *skb, struct net_device *dev) {
     return NETDEV_TX_OK;
 }
 
-static struct net_device_stats *get_stats(struct net_device *dev) {
+static struct net_device_stats *get_stats(struct net_device *dev)
+{
     return &stats;
 }
 
-static struct net_device_ops net_device_ops = {
-        .ndo_open = open,
-        .ndo_stop = stop,
-        .ndo_get_stats = get_stats,
-        .ndo_start_xmit = start_xmit
-};
+static struct net_device_ops net_device_ops = { .ndo_open = open,
+                                                .ndo_stop = stop,
+                                                .ndo_get_stats = get_stats,
+                                                .ndo_start_xmit = start_xmit };
 
-static void setup(struct net_device *dev) {
-    int i;
+static void setup(struct net_device *dev)
+{
+    char i;
     ether_setup(dev);
     memset(netdev_priv(dev), 0, sizeof(struct priv));
     dev->netdev_ops = &net_device_ops;
 
     //fill in the MAC address
-    for (i = 0; i < ETH_ALEN; i++)
-        dev->dev_addr[i] = (char)i;
+    for (i = 0; i < ETH_ALEN; i++) {
+        dev->dev_addr[i] = i;
+    }
 }
 
-int __init vni_init(void) {
+int __init vni_init(void)
+{
     int err = 0;
     struct priv *priv;
     child = alloc_netdev(sizeof(struct priv), ifname, NET_NAME_UNKNOWN, setup);
@@ -115,7 +143,8 @@ int __init vni_init(void) {
         free_netdev(child);
         return -ENODEV;
     }
-    if (priv->parent->type != ARPHRD_ETHER && priv->parent->type != ARPHRD_LOOPBACK) {
+    if (priv->parent->type != ARPHRD_ETHER &&
+        priv->parent->type != ARPHRD_LOOPBACK) {
         printk(KERN_ERR "%s: illegal net type", THIS_MODULE->name);
         free_netdev(child);
         return -EINVAL;
@@ -136,17 +165,20 @@ int __init vni_init(void) {
     rtnl_unlock();
     printk(KERN_INFO "Module %s loaded", THIS_MODULE->name);
     printk(KERN_INFO "%s: create link %s", THIS_MODULE->name, child->name);
-    printk(KERN_INFO "%s: registered rx handler for %s", THIS_MODULE->name, priv->parent->name);
+    printk(KERN_INFO "%s: registered rx handler for %s", THIS_MODULE->name,
+           priv->parent->name);
     return 0;
 }
 
-void __exit vni_exit(void) {
+void __exit vni_exit(void)
+{
     struct priv *priv = netdev_priv(child);
     if (priv->parent) {
         rtnl_lock();
         netdev_rx_handler_unregister(priv->parent);
         rtnl_unlock();
-        printk(KERN_INFO "%s: unregister rx handler for %s", THIS_MODULE->name, priv->parent->name);
+        printk(KERN_INFO "%s: unregister rx handler for %s", THIS_MODULE->name,
+               priv->parent->name);
     }
     unregister_netdev(child);
     free_netdev(child);
